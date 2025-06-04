9267e728069d9f870351dd71b6353691ac509d29
vcanepaa <vinycanepa@gmail.com>
2025-06-03 22:20:08 -0300

    Segunda versao

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Render!'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
